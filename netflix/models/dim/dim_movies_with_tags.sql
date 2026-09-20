6dcf1825728f1b8165f440c3a7287ba402a7475b
Petr Shulgat <petrshulgat@mailbox.unideb.hu>
2026-09-20 13:17:43 +0200

    create dim ephemeral

---
+++ netflix/models/dim/dim_movies_with_tags.sql
@@ -0,0 +1,26 @@
+{{
+    config(
+        materialized = 'ephemeral'
+    )
+}}
+
+with movies as (
+    select * from {{ ref("dim_movies") }}
+),
+tags as (
+    select * from {{ ref("dim_genome_tags") }}
+)
+,
+scres as (
+    select * from {{ ref("fct_genome_scores") }}
+)
+
+SELECT
+    m.movie_id,
+    m.movie_title,
+    m.genres,
+    t.tag_name,
+    s.relevance
+from movies m 
+left join scores s on m.movie_id=s.movie_id
+left join tags t on t.tag_id=s.tag_id
